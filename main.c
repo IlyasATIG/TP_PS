0c2fb7361814cb3d7defa1db4173b1b47125efb1
William BEAUSON <willsecond972@gmail.com>
2025-12-10 15:29:07 +0100

    Q1-Completed

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "question_1.h"
+
+int main() {
+    // Appel de la fonction définie dans le module question_1
+    display_welcome_prompt();
+
+    while (1) {
+        // La suite viendra ici (Question 2)
+        break;
+    }
+    return 0;
+}

0f61a8339e86d22cc66ab8eed7797eebaa744d88
William BEAUSON <willsecond972@gmail.com>
2025-12-17 23:00:15 +0100

    With Q4 completed

--- main.c
+++ main.c
@@ -1,20 +1,30 @@
 #include <stdlib.h>
 #include "question_1.h"
-#include "question_2.h"
+#include "question_4.h"
 
 
 
-int main() {
+//int main() {
 
     // Q1 : Call of  the function defined in question_1.c
+  //  display_welcome_prompt();
+
+    //while (1) {
+        // Q2 : Reading et executing the command
+      //  execute_one_simple_command();
+        
+
+        //display_prompt();
+    //}
+   // return 0;
+//}
+
+int main() {
     display_welcome_prompt();
 
     while (1) {
-        // Q2 : Reading et executing the command
-        execute_one_simple_command();
-        
-
-        display_prompt();
+        display_prompt(); // Affiche le prompt
+        execute_one_simple_command(); // Lit et exécute
     }
     return 0;
 }

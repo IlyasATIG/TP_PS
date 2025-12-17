cfac1369611806f78184a612d7f1f7ceb107dfc2
William BEAUSON <willsecond972@gmail.com>
2025-12-17 23:40:49 +0100

    with Q5 completed

--- main.c
+++ main.c
@@ -1,10 +1,5 @@
-#include <stdlib.h>
-#include "question_1.h"
-#include "question_4.h"
-
-
-
-//int main() {
+/********************* Question 1-2-3 ************************** 
+int main() {
 
     // Q1 : Call of  the function defined in question_1.c
   //  display_welcome_prompt();
@@ -18,13 +13,32 @@
     //}
    // return 0;
 //}
-
+/********************* Question 4 ************************** 
+#include <stdlib.h>
+#include "question_1.h"
+#include "question_4.h"
 int main() {
     display_welcome_prompt();
 
     while (1) {
         display_prompt(); // Affiche le prompt
         execute_one_simple_command(); // Lit et exécute
+    }
+    return 0;
+}
+    */
+
+//********************* Question 5 ************************** 
+#include <stdlib.h>
+#include "question_1.h"
+#include "question_5.h" 
+
+int main() {
+    display_welcome_prompt();
+
+    while (1) {
+        display_prompt();
+        execute_one_simple_command();
     }
     return 0;
 }

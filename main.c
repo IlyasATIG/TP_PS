7c86c1e946da3fd96fca6d5d0b1e6a229222ab8b
William BEAUSON <willsecond972@gmail.com>
2025-12-18 00:14:32 +0100

    with Q6 completed

--- main.c
+++ main.c
@@ -27,11 +27,27 @@ int main() {
     return 0;
 }
     */
-
+/*
 //********************* Question 5 ************************** 
 #include <stdlib.h>
 #include "question_1.h"
 #include "question_5.h" 
+
+int main() {
+    display_welcome_prompt();
+
+    while (1) {
+        display_prompt();
+        execute_one_simple_command();
+    }
+    return 0;
+}
+*/
+
+//********************* Question 6 ************************** 
+#include <stdlib.h>
+#include "question_1.h"
+#include "question_6.h" 
 
 int main() {
     display_welcome_prompt();
